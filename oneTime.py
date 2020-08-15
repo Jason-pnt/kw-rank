d1a25aeb54b26e06416d9604f45f1b25e5a4b800
Jason-pnt <ci@pntsports.cn>
2020-08-15 09:12:16 +0800

    update

--- oneTime.py
+++ oneTime.py
@@ -11,19 +11,11 @@ father_path = os.path.abspath(os.path.dirname(current_path) + os.path.sep + ".")
 
 repo = Repo(os.path.abspath(father_path))
 pdt_timezone=pytz.timezone("America/Los_Angeles")
-flags = True
-while flags:
-    time_now = time.strftime("%H:%M:%S", time.localtime())
-    dates = time.strftime("%Y-%m-%d", time.localtime())
-    # pdt_times = datetime.datetime.now(pdt_timezone).strftime("%Y-%m-%d %H:%M:%S")
-    for i in (os.listdir(os.path.join(father_path, 'keyword'))):
-        if i.endswith('csv'):
-            print(i + '\n')
-            commitName = (i.split('.')[0])
-            commitName = commitName + ' CN: ' + dates
-            # copyfile(os.path.join(father_path, 'keyword', i), os.path.join(father_path, 'test.csv'))
-            repo.index.add(['test.csv'])
-            repo.index.commit(commitName)
-            subprocess.check_call(['git', 'push', 'origin', 'master'])
-            time.sleep(2)
-    flags = False
+
+time_now = time.strftime("%H:%M:%S", time.localtime())
+dates = time.strftime("%Y-%m-%d", time.localtime())
+
+commitName = 'test.csv' + ' CN: ' + dates
+repo.index.add(['test.csv'])
+repo.index.commit(commitName)
+subprocess.check_call(['git', 'push', 'origin', 'master'])            
