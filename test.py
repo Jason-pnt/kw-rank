04d622204feb0ef03e8cd7f0f97c04685fe1fc29
jason-pnt <ci@pntsports.cn>
2020-10-16 18:36:05 +0800

    update

--- test.py
+++ test.py
@@ -36,14 +36,14 @@ print('\n\nNow test begining country: ' + country + ' city: ' + cityName + ' Zip
 
 driver.get('https://www.amazon.com/?currency=USD&language=en_US')
 time.sleep(10)
-driver.find_element_by_xpath('//*[@id="nav-packard-glow-loc-icon"]').click()
-time.sleep(10)
-driver.find_element_by_xpath('//*[@id="GLUXZipUpdateInput"]').send_keys('10001')
-time.sleep(10)
-driver.find_element_by_xpath('//*[@id="GLUXZipUpdate"]/span/input').click()
-time.sleep(10)
-driver.get('https://www.amazon.com/')
-print('\n Amazon ZIPCode:'+ driver.find_element_by_xpath('//*[@id="glow-ingress-line2"]').text)
+# driver.find_element_by_xpath('//*[@id="nav-packard-glow-loc-icon"]').click()
+# time.sleep(10)
+# driver.find_element_by_xpath('//*[@id="GLUXZipUpdateInput"]').send_keys('10001')
+# time.sleep(10)
+# driver.find_element_by_xpath('//*[@id="GLUXZipUpdate"]/span/input').click()
+# time.sleep(10)
+# driver.get('https://www.amazon.com/')
+# print('\n Amazon ZIPCode:'+ driver.find_element_by_xpath('//*[@id="glow-ingress-line2"]').text)
 
 counts = 0
 final_result = {}
