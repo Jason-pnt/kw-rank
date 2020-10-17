3bcbd812bc7ed0a1d83af0df4945b3c7681f3e32
jason-pnt <ci@pntsports.cn>
2020-10-17 20:41:13 +0800

    update

--- launch.sh
+++ launch.sh
@@ -4,10 +4,9 @@ git pull
 git checkout .
 for  n in `find keyword -name "*.xlsx"`
 do
-	echo $n
 	now_time=$(date "+%Y-%m-%d %H:%M:%S")
 	cp $n test.xlsx
 	git add .
-	git commit -am "Index $n $now_time"
+	git commit -am "KW-RANK $n $now_time"
 	git push origin master
 done
